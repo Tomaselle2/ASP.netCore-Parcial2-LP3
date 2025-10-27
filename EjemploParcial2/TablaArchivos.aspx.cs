a8aa7ee497b0334d1b2560f8a5ed5c7b872ede6e
Tomaselle2 <169102423+Tomaselle2@users.noreply.github.com>
2025-10-27 19:56:55 -0300

    Se puede descargar Archivos

--- EjemploParcial2/TablaArchivos.aspx.cs
+++ EjemploParcial2/TablaArchivos.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +12,76 @@ namespace EjemploParcial2
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            cargarGrilla();
         }
+
+        public void cargarGrilla()
+        {
+            string path = $"{Server.MapPath(".")}/files";
+            if (Directory.Exists(path))
+            {
+                string[] files = Directory.GetFiles(path);
+                List<Archivos> fileList = new List<Archivos>();
+
+                foreach (string file in files)
+                {
+                    string nombre = Path.GetFileName(file);
+                    long tamanio = new FileInfo(file).Length / 1024;
+                    string vpath = $"~/files/{nombre}";
+                    var fileNew = new Archivos(nombre, file, vpath, tamanio);
+                    fileList.Add(fileNew);
+                }
+
+                GridView1.DataSource = fileList;
+                GridView1.DataBind();
+            }
+        }
+
+        protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "Descargar")
+            {
+                GridViewRow registro = GridView1.Rows[Convert.ToInt32(e.CommandArgument)];
+                string filePath = registro.Cells[4].Text;
+
+                Response.ContentType = "application/octet-stream";
+                Response.AppendHeader("Content-Disposition", "attachment; filename=" + Path.GetFileName(filePath));
+                Response.TransmitFile(filePath);
+                Response.End();
+            }
+            if (e.CommandName == "Eliminar")
+            {
+                GridViewRow registro = GridView1.Rows[Convert.ToInt32(e.CommandArgument)];
+                string fileName = registro.Cells[2].Text;
+                string fullPath = Path.Combine(Server.MapPath("~/files"), fileName);
+                if (File.Exists(fullPath))
+                {
+                    File.Delete(fullPath);
+                    string script = $@"
+                            Swal.fire({{
+                                title: 'Éxito',
+                                text: '{"Archivo eliminado exitosamente"}',
+                                icon: 'success',
+                                confirmButtonText: 'Aceptar'
+                            }});
+                        ";
+                    ScriptManager.RegisterStartupScript(this, GetType(), "SweetAlert", script, true);
+                }
+                else
+                {
+                    string script = $@"
+                        Swal.fire({{
+                            title: 'Error',
+                            text: '{$"El archivo '{fileName}' no existe o ya fue eliminado."}',
+                            icon: 'error',
+                            confirmButtonText: 'Aceptar'
+                        }});
+                    ";
+                    ScriptManager.RegisterStartupScript(this, GetType(), "SweetAlert", script, true);
+                }
+                cargarGrilla();
+            }
+        }
+    
     }
 }
--- EjemploParcial2/TablaArchivos.aspx.designer.cs
+++ EjemploParcial2/TablaArchivos.aspx.designer.cs
@@ -15,6 +15,15 @@ namespace EjemploParcial2
     {
 
         /// <summary>
+        /// Control ScriptManager3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.ScriptManager ScriptManager3;
+
+        /// <summary>
         /// Control GridView1.
         /// </summary>
         /// <remarks>
